c89fd5f9e2ac3de25bea79dc0d06574b04a9ab34
Alfredo Enrione <aenrione@protonmail.com>
2021-11-04 12:45:17 -0300

    edit buda too add invested amount per crypto

--- entities/buda_api.py
+++ entities/buda_api.py
@@ -20,12 +20,24 @@ class BudaApi():
         output = ""
 
         for crypto in BUDA_CRYPTO_LIST:
+            market = self.client.getMarket(f"{crypto}-CLP")
+            orders = market.getTradedOrders()
+            invested = 0
+            for order in orders:
+                invested += self.__get_order_invested_value(order)
             conversion_rate = self.client \
                 .getTicker(f"{crypto}-CLP")['last_price'][BUDA_USEFULL_INDEX]
             crypto_balance = self.client \
                 .getBalance(crypto)["amount"][BUDA_USEFULL_INDEX]
             tmp_balance = float(conversion_rate)*float(crypto_balance)
             balance += tmp_balance
-            output += f"\n ${tmp_balance:,.0f} in {crypto} ({self.name})"
+            i_s = f"(of ${invested:,.0f} CLP)"
+            output += f"\n ${tmp_balance:,.0f} {i_s} in {crypto} ({self.name})"
 
         return balance, output
+
+    def __get_order_invested_value(self, order):
+        tmp_exchanged = float(order["total_exchanged"][BUDA_USEFULL_INDEX])
+        if order["type"] == "Ask":
+            tmp_exchanged = -tmp_exchanged
+        return tmp_exchanged
